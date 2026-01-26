5f754917ed0e1b07f9874063f8a8c57d6c8cadc9
ProjeniusOfficial <projenius2025@gmail.com>
2026-01-27 00:54:52 +0530

    Fix mongoose connection for production

--- server.js
+++ server.js
@@ -26,12 +26,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
     console.log("MongoDB Connected");
     app.listen(PORT, () =>
       console.log(`Server running on port ${PORT}`)
